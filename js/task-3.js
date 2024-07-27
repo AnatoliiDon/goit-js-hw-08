33c86c5e6e6a0708c02478c91eb00f5044512edb
Anatoliy Don <anatoliy_don@MacBook-Air-Anatoliy.local>
2024-07-27 19:21:24 +0300

    try1

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-function getElementWidth(content, padding, border) {
-  const result =
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-  return result;
-}
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
-console.log(getElementWidth('146px', '12.5px', '18.5px'));
